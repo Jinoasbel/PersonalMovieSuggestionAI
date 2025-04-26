7a874caece4c0cb6653d27c016df5f6a74309838
Jinoasbel <jinoasbel@gmail.com>
2025-04-26 22:11:10 +0530

    codes have written to Recieve movie details

---
+++ ApiManager.py
@@ -0,0 +1,31 @@
+# https://random-word-api.herokuapp.com/word?number=42 
+
+# Here is your key: 9f5d9eb3
+
+# Please append it to all of your API requests,
+
+# OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=9f5d9eb3
+
+import requests
+
+class ApiManager:
+
+    ApiKey = "9f5d9eb3"
+    SafetyInt = 10
+
+    def RandomWordGenerator(self):
+
+        RndWordApi = "https://random-word-api.herokuapp.com/word?"
+        return requests.get(RndWordApi).json()[0]
+        
+    
+    def UpdateList(self):
+
+        for i in range (0, self.SafetyInt):
+            Title = self.RandomWordGenerator()
+            OmdbApiUrl = f"http://www.omdbapi.com/?s={Title}&apikey={self.ApiKey}"
+            Response = requests.get(OmdbApiUrl)
+            print(Response.json())
+
+obj = ApiManager()
+obj.UpdateList()
