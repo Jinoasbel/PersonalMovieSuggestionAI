13a0d05a20be6515877abc0a4211fadbfbc12dec
Jinoasbel <jinoasbel@gmail.com>
2025-05-01 09:19:02 +0530

    some changes have been made on the filemanager

--- VectorComputing.py
+++ VectorComputing.py
@@ -14,31 +14,14 @@ class VectorComputing:
     "Tom Holland",
     "Pedro Pascal"
 ]
-    Genre = [
-    "Action",
-    "Comedy",
-    "Drama",
-    "Thriller",
-    "Horror",
-    "Science Fiction",
-    "Romance",
-    "Fantasy",
-    "Adventure",
-    "Crime"
-]
-    Genre_ids = [
-    28, 12, 16, 35, 80, 99, 18, 10751,
-    14, 36, 27, 10402, 9648, 10749, 878,
-    10770, 53, 10752, 37
-]
 
     GenreVector = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    Genre_names = [
-    "Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary",
-    "Drama", "Family", "Fantasy", "History", "Horror", "Music",
-    "Mystery", "Romance", "Science Fiction", "TV Movie",
-    "Thriller", "War", "Western"
-]
+    Genre = {
+    28: 'Action', 12: 'Adventure', 16: 'Animation', 35: 'Comedy', 80: 'Crime', 99: 'Documentary',
+    18: 'Drama', 10751: 'Family', 14: 'Fantasy', 36: 'History', 27: 'Horror', 10402: 'Music',
+    9648: 'Mystery', 10749: 'Romance', 878: 'Science Fiction', 10770: 'TV Movie', 53: 'Thriller',
+    10752: 'War', 37: 'Western'
+    }
 
 
 
